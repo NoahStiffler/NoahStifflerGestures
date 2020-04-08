4a874c3d1931f4f6bea4b1aabd543cd143b7dd79
Noah Stiffler <stifflernoah@gmail.com>
2020-04-08 09:58:15 -0400

    Initial Commit

---
+++ NoahStifflerGestures/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NoahStifflerGestures
+//
+//  Created by Noah Stiffler on 4/8/20.
+//  Copyright © 2020 Noah Stiffler. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
